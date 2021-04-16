28ece841a32407bcb4ee8ffce7b7fe63bd4e2252
KVANTORIANEC <q2q2>
2021-04-16 19:09:29 +0300

    qw

--- core/src/com/diggers/game/DiggersGame.java
+++ core/src/com/diggers/game/DiggersGame.java
@@ -7,10 +7,12 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.diggers.game.units.Platform;
 import com.diggers.game.units.Unit;
 import com.diggers.game.units.UnitPhysic;
 
+import java.awt.Rectangle;
 import java.util.ArrayList;
 
 public class DiggersGame extends ApplicationAdapter {
@@ -18,14 +20,45 @@ public class DiggersGame extends ApplicationAdapter {
 	Unit unit;
 	ArrayList<Platform> platforms = new ArrayList<>();
 
+	AnimatedSprite h;
+
 	Sprite stena;
 
 	int p = 0;
+
+	class AnimatedSprite{
+		float FRAME_DURATION = 0.2f;
+
+		Texture texture;
+		Rectangle curFrameRect;
+		int frameWidth = 100;
+		int frameHeight = 100;
+		float curFrameTimer = 0;
+
+		public AnimatedSprite(Texture texture, Rectangle curFrameRect) {
+			this.texture = texture;
+			this.curFrameRect = curFrameRect;
+		}
+
+		void update(float deltaTime){
+			curFrameTimer += deltaTime;
+			if (curFrameTimer >= FRAME_DURATION){
+				curFrameRect = new Rectangle((curFrameRect.x + frameWidth) % (5 * frameWidth), curFrameRect.y, curFrameRect.width, curFrameRect.height);
+				curFrameTimer = 0;
+			}
+		}
+
+		void draw(SpriteBatch batch){
+			Sprite sprite = new Sprite(texture, (int) curFrameRect.x, (int) curFrameRect.y, (int) curFrameRect.width, (int) curFrameRect.height);
+			batch.draw(sprite, 0, 0);
+		}
+	}
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 
+		h = new AnimatedSprite(new Texture("g.png"), new Rectangle(0, 0, 80, 100));
 		stena = new Sprite(new Texture("stena.png"));
 		unit = new Unit("geroipravo.png", 200, 300);
 
@@ -64,24 +97,25 @@ public class DiggersGame extends ApplicationAdapter {
 		}
 
 		unit.update(Gdx.graphics.getDeltaTime(), platforms);
-
+		h.update(Gdx.graphics.getDeltaTime());
 		batch.begin();
+		h.draw(batch);
 
-		for (int i = 0; i < Gdx.graphics.getWidth() / stena.getWidth() + 1; i++){
-			for(int j = 0; j < Gdx.graphics.getHeight() / stena.getHeight() + 1; j++){
-				stena.setX(i * stena.getWidth());
-				stena.setY(j * stena.getHeight());
-				stena.draw(batch);
-			}
-		}
-
-
-		unit.draw(batch, Gdx.graphics.getDeltaTime());
+//		for (int i = 0; i < Gdx.graphics.getWidth() / stena.getWidth() + 1; i++){
+//			for(int j = 0; j < Gdx.graphics.getHeight() / stena.getHeight() + 1; j++){
+//				stena.setX(i * stena.getWidth());
+//				stena.setY(j * stena.getHeight());
+//				stena.draw(batch);
+//			}
+//		}
 
 
-		for (Platform platform: platforms){
-			platform.draw(batch, Gdx.graphics.getDeltaTime());
-		}
+//		unit.draw(batch, Gdx.graphics.getDeltaTime());
+//
+//
+//		for (Platform platform: platforms){
+//			platform.draw(batch, Gdx.graphics.getDeltaTime());
+//		}
 
 		batch.end();
 	}
